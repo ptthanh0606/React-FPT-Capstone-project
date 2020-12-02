170871730f6342e2e7d3d22aeeadd98a9aad5ae9
Phan Thông Thành <phanthongthanh0606@gmail.com>
2020-12-03 04:48:15 +0700

    Fix hover button on dashboard

--- src/components/CMSWidgets/QuickAction/ActionCard.js
+++ src/components/CMSWidgets/QuickAction/ActionCard.js
@@ -21,12 +21,12 @@ const ActionCard = ({
     <a
       href="/"
       onClick={handleClick}
-      className={`col px-6 py-8 rounded-xl bg-light-${type} ${className}`}
+      className={`col px-6 py-8 rounded-xl bg-${type} ${className}`}
     >
-      <span className={`svg-icon svg-icon-3x svg-icon-${type} d-block my-2`}>
+      <span className={`svg-icon svg-icon-3x svg-icon-white d-block my-2`}>
         <SVG src={iconSrc}></SVG>
       </span>
-      <span className={`text-${type} font-weight-bold font-size-h6 mt-2`}>
+      <span className={`text-white font-weight-bold font-size-h6 mt-2`}>
         {label}
       </span>
     </a>
