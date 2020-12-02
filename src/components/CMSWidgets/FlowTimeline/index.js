4156e72b927b50bb5bf3d3253ace6539768d875f
Phan Thông Thành <phanthongthanh0606@gmail.com>
2020-12-03 02:37:35 +0700

    update flowTimeLine widget, re-layout lecturer dashboard widgets

--- src/components/CMSWidgets/FlowTimeline/index.js
+++ src/components/CMSWidgets/FlowTimeline/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import TimelineItem from './FlowItem';
 
-const FlowTimeline = ({ className }) => {
+const FlowTimeline = ({
+  className,
+  items = [
+    {
+      date: '12 Jun',
+      content: (
+        <>
+          <div className="font-weight-bolder font-size-lg timeline-content pl-3">
+            Test
+          </div>
+        </>
+      ),
+    },
+  ],
+}) => {
   return (
     <div className={`card card-custom ${className}`}>
       <div className="card-header align-items-center border-0 mt-4">
@@ -14,117 +28,13 @@ const FlowTimeline = ({ className }) => {
       </div>
       <div className="card-body pt-4">
         <div className="timeline timeline-6 mt-3">
-          <TimelineItem
-            date="12 Jun"
-            content={
-              <div className="font-weight-bolder font-size-lg timeline-content pl-3">
-                Start in-capstone phase
-              </div>
-            }
-          />
-          <TimelineItem
-            date="12 Aug"
-            content={
-              <div className="timeline-content flex-row d-flex">
-                <span className="font-weight-normal text-muted text-dark-75 pl-3 font-size-lg mr-5">
-                  Checkpoint meeting with{' '}
-                  <span className="font-weight-bolder text-dark-75">
-                    Lam Huu Khanh Phuong, Tran Tuan Anh
-                  </span>
-                </span>
-              </div>
-            }
-          />
-          <TimelineItem
-            date="12 May"
-            content={
-              <div className="timeline-content font-weight-bolder font-size-lg text-dark-75 pl-3">
-                Send report
-                <a href="/" className="text-primary ml-1">
-                  #1
-                </a>
-              </div>
-            }
-          />
-          <TimelineItem
-            date="12 May"
-            content={
-              <div className="timeline-content font-weight-bolder font-size-lg text-dark-75 pl-3">
-                Send report
-                <a href="/" className="text-primary ml-1">
-                  #2
-                </a>
-              </div>
-            }
-          />
-          <TimelineItem
-            date="12 May"
-            content={
-              <div className="timeline-content font-weight-bolder font-size-lg text-dark-75 pl-3">
-                Send report
-                <a href="/" className="text-primary ml-1">
-                  #3
-                </a>
-              </div>
-            }
-          />
-          <TimelineItem
-            date="12 May"
-            content={
-              <div className="timeline-content font-weight-mormal font-size-lg text-muted pl-3">
-                Some thing to be on the timeline
-              </div>
-            }
-          />
-          <TimelineItem
-            date="12 May"
-            content={
-              <div className="timeline-content font-weight-bolder font-size-lg text-dark-75 pl-3">
-                Send report
-                <a href="/" className="text-primary ml-1">
-                  #4
-                </a>
-              </div>
-            }
-          />
-          <TimelineItem
-            date="12 May"
-            content={
-              <div className="timeline-content font-weight-bolder font-size-lg text-dark-75 pl-3">
-                Send report
-                <a href="/" className="text-primary ml-1">
-                  #5
-                </a>
-              </div>
-            }
-          />
-          <TimelineItem
-            date="12 May"
-            content={
-              <div className="timeline-content font-weight-bolder font-size-lg text-dark-75 pl-3">
-                Send report
-                <a href="/" className="text-primary ml-1">
-                  #6
-                </a>
-              </div>
-            }
-          />
-          <TimelineItem
-            date="12 May"
-            content={
-              <div className="timeline-content font-weight-mormal font-size-lg text-muted pl-3">
-                Some thing to be on the timeline
-              </div>
-            }
-          />
-          <TimelineItem
-            date="12 May"
-            content={
-              <div className="font-weight-bolder font-size-lg timeline-content pl-3">
-                End capstone semester
-              </div>
-            }
-          />
+          {items?.length ? (
+            items.map(item => (
+              <TimelineItem date={item.date} content={item.content} />
+            ))
+          ) : (
+            <>Nothing yet...</>
+          )}
         </div>
       </div>
     </div>
--- src/views/user/LecturerDashboard/index.js
+++ src/views/user/LecturerDashboard/index.js
@@ -10,12 +10,14 @@ import { applicationRowActionFormatter, rowActionFormatter } from './constants';
 import StatTile from 'components/CMSWidgets/StatTile';
 import { useHistory } from 'react-router-dom';
 import DropdownPopover from 'components/DropdownPopover';
+import FlowTimeline from 'components/CMSWidgets/FlowTimeline';
 
 export default React.memo(function LecturerDashboard() {
   const history = useHistory();
   const setMeta = useSetRecoilState(metaAtom);
   const [topicType, setTopicType] = React.useState('Submited');
   const [topicPreviews, setTopicPreviews] = React.useState([]);
+  const [flowTimelines, setFlowTimelines] = React.useState([]);
 
   // --------------------------------------------------------------------
 
@@ -121,6 +123,51 @@ export default React.memo(function LecturerDashboard() {
     setTopicPreviews(response);
   }, [handleRouteToSpecificTopic, topicType]);
 
+  React.useEffect(() => {
+    setFlowTimelines([
+      {
+        date: '12 Jun',
+        content: (
+          <div className="font-weight-bolder font-size-lg timeline-content pl-3">
+            Start in-capstone phase
+          </div>
+        ),
+      },
+      {
+        date: '12 Jun',
+        content: (
+          <div className="timeline-content flex-row d-flex">
+            <span className="font-weight-normal text-muted text-dark-75 pl-3 font-size-lg mr-5">
+              Checkpoint meeting with{' '}
+              <span className="font-weight-bolder text-dark-75">
+                Lam Huu Khanh Phuong, Tran Tuan Anh
+              </span>
+            </span>
+          </div>
+        ),
+      },
+      {
+        date: '12 Jun',
+        content: (
+          <div className="timeline-content font-weight-bolder font-size-lg text-dark-75 pl-3">
+            Send report
+            <a href="/" className="text-primary ml-1">
+              #1
+            </a>
+          </div>
+        ),
+      },
+      {
+        date: '12 Jun',
+        content: (
+          <div className="timeline-content font-weight-mormal font-size-lg text-muted pl-3">
+            Some thing to be on the timeline
+          </div>
+        ),
+      },
+    ]);
+  }, []);
+
   return (
     <div className="row">
       <div className="col-lg-6 col-xxl-4">
@@ -153,6 +200,60 @@ export default React.memo(function LecturerDashboard() {
           </div>
         </div>
         <div className="row">
+          <div className="col-lg-12 col-xxl-12">
+            <Anouncement
+              className="gutter-b"
+              date="20 Jun 2020"
+              body={
+                <>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  <br />
+                  <br /> Duis aute irure dolor in reprehenderit in voluptate
+                  velit esse cillum dolore eu fugiat nulla pariatur. <br />
+                </>
+              }
+            />
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-6 col-xxl-4">
+        <div className="row">
+          <div className="col-lg-12 col-xxl-12">
+            <FlowTimeline className="gutter-b" items={flowTimelines} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12 col-xxl-12">
+            <CMSList
+              className="gutter-b"
+              title="Topic applications"
+              rows={[
+                {
+                  label: 'Capstone Management System',
+                  onLabelClick: handleRouteToSpecificTopic(0),
+                  subLabel: 'FA20SE13',
+                  action: applicationRowActionFormatter(10),
+                },
+                {
+                  label: 'Web Checker System',
+                  onLabelClick: handleRouteToSpecificTopic(0),
+                  subLabel: 'FA20SE11',
+                  action: applicationRowActionFormatter(5),
+                },
+                {
+                  label: 'Example topic name',
+                  onLabelClick: handleRouteToSpecificTopic(0),
+                  subLabel: 'FA20SE15',
+                  action: applicationRowActionFormatter(2),
+                },
+              ]}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-6 col-xxl-4">
+        <div className="row">
           <div className="col-lg-6 col-xxl-6">
             <StatTile
               className="gutter-b"
@@ -178,68 +279,31 @@ export default React.memo(function LecturerDashboard() {
             />
           </div>
         </div>
-      </div>
-      <div className="col-lg-6 col-xxl-4">
-        <CMSList
-          className="gutter-b"
-          title="My topic"
-          toolBar={
-            <DropdownPopover
-              value="Submited"
-              items={[
-                {
-                  label: 'Submited',
-                  value: 'Submited',
-                },
-                {
-                  label: 'Mentoring',
-                  value: 'Mentoring',
-                },
-              ]}
-              onChange={value => setTopicType(value)}
+        <div className="row">
+          <div className="col-lg-12 col-xxl-12">
+            <CMSList
+              className="gutter-b"
+              title="My topic"
+              toolBar={
+                <DropdownPopover
+                  value="Submited"
+                  items={[
+                    {
+                      label: 'Submited',
+                      value: 'Submited',
+                    },
+                    {
+                      label: 'Mentoring',
+                      value: 'Mentoring',
+                    },
+                  ]}
+                  onChange={value => setTopicType(value)}
+                />
+              }
+              rows={topicPreviews}
             />
-          }
-          rows={topicPreviews}
-        />
-      </div>
-      <div className="col-lg-6 col-xxl-4">
-        <Anouncement
-          className="gutter-b"
-          date="20 Jun 2020"
-          body={
-            <>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              <br />
-              <br /> Duis aute irure dolor in reprehenderit in voluptate velit
-              esse cillum dolore eu fugiat nulla pariatur. <br />
-            </>
-          }
-        />
-        <CMSList
-          className="gutter-b"
-          title="Topic applications"
-          rows={[
-            {
-              label: 'Capstone Management System',
-              onLabelClick: handleRouteToSpecificTopic(0),
-              subLabel: 'FA20SE13',
-              action: applicationRowActionFormatter(10),
-            },
-            {
-              label: 'Web Checker System',
-              onLabelClick: handleRouteToSpecificTopic(0),
-              subLabel: 'FA20SE11',
-              action: applicationRowActionFormatter(5),
-            },
-            {
-              label: 'Example topic name',
-              onLabelClick: handleRouteToSpecificTopic(0),
-              subLabel: 'FA20SE15',
-              action: applicationRowActionFormatter(2),
-            },
-          ]}
-        />
+          </div>
+        </div>
       </div>
     </div>
   );
